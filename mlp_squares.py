753712404f5c2660275a67532845cda5670eab81
abc <abc@example.com>
2019-09-07 19:34:17 +0200

    first commit

---
+++ mlp_squares.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+'''
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, squares, linewidth=5)
+
+# Name and title for a graph
+plt.title("Squares", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Squres value", fontsize=14)
+
+# Zdefiniowanie wielkosci etykiet
+plt.tick_params(axis='both', labelsize=14)
+
+
+#plt.plot(squares)
+plt.show()
+'''
+plt.scatter(2,4)
+plt.show()
